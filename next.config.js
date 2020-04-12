b1dc473d6f4b1b669594c0b58ec11e23736965e9
Rim Vilgalys <vilgalys@gmail.com>
2020-04-12 15:32:32 -0400

    commit before more changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-const withCSS = require("@zeit/next-css");
 const withImages = require("next-images");
 
-module.exports = withImages(
-  withCSS({
-    env: {
-      TEST: process.env.TEST
-    }
-  })
-);
+module.exports = withImages({});
--- pages/app.js
+++ pages/app.js
@@ -1,7 +1,5 @@
 import Layout from "../components/Layout";
 
-import "../styles/app.css";
-
 const App = () => (
   <Layout>
     <main className="h-screen w-screen bg-topo flex flex-col items-center justify-start py-12">
@@ -10,15 +8,29 @@ const App = () => (
           The Social Distance Network
         </h1>
       </section>
-      <section id="app-main" className="text-gray-100 font-body">
-        <div id="channel">
+      <section
+        id="app-main"
+        className="text-gray-100 font-body flex flex-col justify-start items-center w-full px-8"
+      >
+        <div
+          id="channel"
+          className="border-2 border-gray-300 h-12 w-full bg-dark flex flex-row items-center justify-center"
+        >
           <span>#channel</span>
         </div>
-        <div id="read">
-          <span>read</span>
-        </div>
-        <div id="write">
-          <span>write</span>
+        <div className="flex flex-row items-start justify-evenly w-full">
+          <div
+            id="read"
+            className="flex-1 flex flex-col items-center justify-start border-2 border-gray-300 h-64 bg-dark"
+          >
+            <span>read</span>
+          </div>
+          <div
+            id="write"
+            className="flex flex-1 flex-col items-center justify-start border-2 border-gray-300 h-64 bg-dark"
+          >
+            <span>write</span>
+          </div>
         </div>
       </section>
     </main>
